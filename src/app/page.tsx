1ea43974398e50eede5b9c1dc46d036325d21a02
Emanuele Simula <simulaemanuele@gmail.com>
2025-01-20 20:12:29 +0100

    feat: updated HiveCell.tsx component and HiveCell.css style sheet. Now the component have several variation by passing props like rotation, and using some methods to nav and handle the text and compose it. Added the children props renderer into the component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,71 @@
+"use client";
+
 import HiveCell from "@/components/HiveCell/HiveCell";
+import { useState } from "react";
 
 export default function Home() {
+  const [visible, setVisible] = useState<boolean>(true);
+
+  console.log("visible: ", visible);
+
   return (
     <div>
-      <h1>Test</h1>
+      <h1
+        style={{ fontWeight: "bolder", fontSize: "5rem", textAlign: "center" }}
+      >
+        Design System
+      </h1>
       <div
         style={{
           display: "flex",
           justifyContent: "space-evenly",
           alignItems: "center",
+          flexWrap: "wrap",
           height: "95vh",
         }}
       >
         <HiveCell
-          dimension="15rem"
+          dimension="20rem"
           type="logo-menu"
           textContent=""
           backgroundVariant={false}
+          setVisible={setVisible}
         />
         <HiveCell
-          dimension="15rem"
+          dimension="20rem"
           type="menu-tile"
-          textContent="TextContent"
+          textContent="Web App"
           backgroundVariant={false}
         />
         <HiveCell
-          dimension="15rem"
+          dimension="20rem"
           type="menu-tile"
           textContent="TextContent 2"
           backgroundVariant={true}
         />
         <HiveCell
-          dimension="15rem"
-          type="rotate"
+          dimension="20rem"
+          type="menu-tile"
+          textContent="TEST"
+          backgroundVariant={true}
+        />
+        <HiveCell
+          dimension="20rem"
+          type=""
           textContent=""
           backgroundVariant={true}
-          rotate="-30deg"
+          rotate={true}
+          rotationValue="-30deg"
+          children={
+            <button onClick={() => console.log("TEST")}>Press me</button>
+          }
+        />
+        <HiveCell
+          dimension="20rem"
+          type="exit"
+          textContent=""
+          backgroundVariant={false}
+          setVisible={setVisible}
         />
       </div>
     </div>
--- src/components/HiveCell/HiveCell.tsx
+++ src/components/HiveCell/HiveCell.tsx
@@ -1,88 +1,134 @@
 "use client";
 
-import React from "react";
+import React, {
+  Children,
+  Dispatch,
+  memo,
+  ReactNode,
+  SetStateAction,
+} from "react";
 import "@/components/HiveCell/HiveCell.css";
 import Image from "next/image";
 import mainLogo from "../../../public/img/logo-BEeYELLOW-png.png";
+import exitIcon from "../../../public/svg/cross.svg";
 import { useRouter } from "next/navigation";
 
-const HiveCell = ({
-  dimension,
-  type,
-  textContent,
-  backgroundVariant,
-  rotate,
-}: {
-  dimension: string;
-  type: string;
-  textContent: string;
-  backgroundVariant: boolean;
-  rotate?: string;
-}) => {
-  const router = useRouter();
-
-  const handleNavigation = (menuText: string): void => {
-    router.push(`/${menuText}`);
-  };
-
-  if (type === "logo-menu") {
-    return (
-      <div
-        className="hexagon"
-        style={{
-          width: dimension,
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <div>
-          <Image priority src={mainLogo} alt="main logo" />
-        </div>
-      </div>
-    );
-  }
-
-  if (type == "menu-tile") {
-    return (
-      <div
-        className="hexagon hive-tile"
-        style={{
-          backgroundColor: `${backgroundVariant ? "#975f00" : "#ffcb73"}`,
-          width: dimension,
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          cursor: "pointer",
-        }}
-      >
-        <div>
-          <div onClick={() => handleNavigation(textContent)}>{textContent}</div>
-        </div>
-      </div>
-    );
-  }
-
-  if (type == "rotate") {
-    return (
-      <div
-        className="hexagon hive-tile"
-        style={{
-          backgroundColor: `${backgroundVariant ? "#975f00" : "#ffcb73"}`,
-          width: dimension,
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          cursor: "pointer",
-          transform: `rotate(${rotate})`,
-        }}
-      >
-        <div>
-          <div onClick={() => handleNavigation(textContent)}>{textContent}</div>
-        </div>
-      </div>
-    );
-  }
+type IConfigType = {
+  logoMenu: string;
+  menuTile: string;
+  exit: string;
 };
+
+const typeConfig: IConfigType = {
+  logoMenu: "logo-menu",
+  menuTile: "menu-tile",
+  exit: "exit",
+};
+
+const HiveCell = memo(
+  ({
+    dimension,
+    type,
+    textContent,
+    backgroundVariant,
+    rotate,
+    rotationValue,
+    setVisible,
+    children,
+  }: {
+    dimension: string;
+    type: string;
+    textContent: string;
+    backgroundVariant: boolean;
+    rotate?: boolean;
+    rotationValue?: string;
+    setVisible?: Dispatch<SetStateAction<boolean>>;
+    children?: ReactNode;
+  }) => {
+    const router = useRouter();
+
+    const handleNavigation = (menuText: string): void => {
+      router.push(`/${menuText}`);
+    };
+
+    const handleVisibleMenu = () => {
+      if (setVisible) setVisible(type === typeConfig.logoMenu ? true : false);
+    };
+
+    const renderText = (text: string): string[] => {
+      let newText = [""];
+      if (text && text.length > 0) {
+        newText = text.split(" ");
+      }
+
+      return newText;
+    };
+
+    const composeRendererText = (textArray: string[]) => {
+      return (
+        <>
+          {textArray.map((text: string, i) => (
+            <div key={i + text}>{text}</div>
+          ))}
+        </>
+      );
+    };
+
+    const renderContent = () => {
+      if (Object.values(typeConfig).includes(type)) {
+        switch (type) {
+          case "logo-menu":
+            return (
+              <div onClick={handleVisibleMenu}>
+                <Image priority src={mainLogo} alt="main logo" />
+              </div>
+            );
+
+          case "menu-tile":
+            return (
+              <div
+                onClick={() => handleNavigation(textContent)}
+                className="text-wrapper"
+              >
+                {composeRendererText(renderText(textContent))}
+              </div>
+            );
+
+          case "exit":
+            return (
+              <div onClick={handleVisibleMenu}>
+                <Image priority src={exitIcon} alt="exit icon" />
+              </div>
+            );
+
+          default:
+            return null;
+        }
+      } else {
+        return <div>{children}</div>;
+      }
+    };
+
+    const baseStyle = {
+      width: dimension,
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      cursor: "pointer",
+      backgroundColor: backgroundVariant ? "#975f00" : "#ffcb73",
+      transform: rotate ? `rotate(${rotationValue})` : undefined,
+      fontSize: "3.125rem",
+    };
+
+    return (
+      <div
+        className={`hexagon ${type === typeConfig.menuTile ? "hive-tile" : ""}`}
+        style={baseStyle}
+      >
+        {renderContent()}
+      </div>
+    );
+  }
+);
 
 export default HiveCell;
